b7d82a40e0b011deb9a32f5cb7a75d44e8495e96
Alexandre Rimthong <rimthong@gmail.com>
2015-03-05 06:36:26 -0500

    Valuable lessons about needing react 0.13b to extend react classes + babel automatically handling jsx

---
+++ js/components/App.jsx.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import EchecMosaic from './EchecMosaic.jsx'
+
+React.render(
+  React.createElement(EchecMosaic),
+  document.getElementById('echecs-mosaic')
+);
--- js/components/EchecMosaic.jsx.js
+++ js/components/EchecMosaic.jsx.js
@@ -3,16 +3,26 @@ import EchecTile from './EchecTile.jsx'
 
 export default class EchecMosaic extends React.Component {
 
+  constructor(props) {
+    super(props);
+    var echecs = [
+      {id:31, name:'Beer fail', image:'http://www.echec.ca/images/31.jpg'},
+      {id:32, name:'Justin delete', image:'http://www.echec.ca/images/32.jpg'},
+      {id:15, name:'sudo rm *', image:'http://www.echec.ca/images/15.jpg'},
+      {id:26, name:'so many stickies', image:'http://www.echec.ca/images/26.jpg'}
+      ]
+    this.state = {echecs: echecs};
+  }
+
   render() {
     return (
       <div className='container'>{
-        this.props.data.echecs
-          .toList()
+        this.state.echecs
           .map(echec =>
             <EchecTile 
-              key={echec.get('id')}
-              name={echec.get('name')}
-              image={echec.get('image')} />
+              key={echec.id}
+              name={echec.name}
+              image={echec.image} />
           )
       }</div>
     )
