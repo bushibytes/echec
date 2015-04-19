ed9e86d6c1c82d087b6471370281c571f43f515f
Alexandre Rimthong <rimthong@gmail.com>
2015-04-19 18:47:50 -0400

    Now we fetch our data from the backend.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var views = require('co-views');
 var less = require('koa-less');
 var router = require('koa-router')();
 var serve = require('koa-static');
+var json = require('koa-json');
+
 
 // x-response-time
 
@@ -30,19 +32,28 @@ app.use(function *(next){
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
+// koa-json for the backend
+app.use(json());
 
-// Static files
+
+// Static files + using less for our css
 app.use(less('static/'));
 app.use(serve('static/'));
 
 // response
-
 router.get('/', function *(next) {
   this.body = yield render('index');
 });
 
-router.get('/goodbye', function *(next) {
-  this.body = yield render('goodbye');
+// API
+router.get('/api/echecs', function *(next) {
+  var echecs = [
+    {id:31, name:'Beer fail', image:'http://www.echec.ca/images/31.jpg'},
+    {id:32, name:'Justin delete', image:'http://www.echec.ca/images/32.jpg'},
+    {id:15, name:'sudo rm *', image:'http://www.echec.ca/images/15.jpg'},
+    {id:26, name:'so many stickies', image:'http://www.echec.ca/images/26.jpg'}
+  ];
+  this.body = echecs;
 });
 
 app.listen(3000);
